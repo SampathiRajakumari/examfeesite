ede759864e497d966a1d4d271aab9cdeadce6374
Your Name <your@email.com>
2025-09-10 15:39:49 +0530

    Update app.py for Render deployment and Razorpay integration

--- app.py
+++ app.py
@@ -307,5 +307,6 @@ def home():
 
 # -------------------- Run App --------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # use Render's port or default 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+   port = int(os.environ.get("PORT", 5000)) 
+   app.run(host="0.0.0.0", port=port, debug=True)
+
